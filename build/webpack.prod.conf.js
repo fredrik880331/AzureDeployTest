23853532940349ca8ba431da963964d854f0d615
Fredrik Andersson <fredrik880331@gmail.com>
2017-11-11 16:47:49 +0100

    template: 'dist/index.html', in webpack.prod.conf

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,7 +53,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: 'index.html',
+      template: 'dist/index.html',
       inject: true,
       minify: {
         removeComments: true,
